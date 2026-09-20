1806e2c221799188872f4fefc4236e1118c92533
Momo153-sys <mohameddoum2000@gmail.com>
2026-09-20 04:26:48 +0300

    Setting up project for Vercel Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { devtools } from "@tanstack/devtools-vite"
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import viteReact from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
-import netlify from "@netlify/vite-plugin-tanstack-start"
 import contentCollections from "@content-collections/vite"
 
 const config = defineConfig({
@@ -18,7 +17,6 @@ const config = defineConfig({
 
   plugins: [
     devtools(),
-    netlify(),
     contentCollections(),
     tailwindcss(),
     tanstackStart(),
